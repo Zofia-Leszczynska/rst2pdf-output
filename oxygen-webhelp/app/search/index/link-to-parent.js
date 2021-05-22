ae4962483c250e9eb6966c85485a9ff894853978
Zofia-Leszczynska <zleszczynska98@gmail.com>
2021-05-22 15:25:27 +0200

    Introduction

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {20:-1,27:-1,2:-1,3:-1,6:-1,18:-1,21:-1,28:-1};
+return {6:-1,21:-1,18:-1,28:-1,20:-1,27:-1,2:-1,3:-1};
 });
